04c92c3d6a6b974d4c6be8f8d24b192b0db50134
redBOY <43438363+redBOY@users.noreply.github.com>
2022-05-26 21:02:43 +0530

    Create digitsrepeatedtwice.sh

---
+++ digitsrepeatedtwice.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -x
+function store() {
+count=0;
+	for((i=0; i<100; i++))
+	do
+		arr[count++]=$i;
+	done
+}
+
+function find_double() {	
+count=0;
+	for(( i=0; i<${#arr[@]}; i++ ))
+	do
+		if [ $((${arr[$i]} % 11)) -eq 0 ] && [ $((${arr[$i]} / 11)) -ne 0 ]
+		then
+			new_arr[count++]=${arr[$i]};
+		fi
+	done
+}
+store
+find_double
+echo ${new_arr[@]}
