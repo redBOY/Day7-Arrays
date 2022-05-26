71c984c84bf51adf064736517ef6e551b3d9c513
redBOY <43438363+redBOY@users.noreply.github.com>
2022-05-26 20:31:25 +0530

    Create threedigsort.sh

---
+++ threedigsort.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -x
+
+n=10;
+function arraygen() {
+count=0
+for (( i=1; i<=$n; i++ ))
+do
+low=100
+high=999
+random[((count++))]=$(( $low + RANDOM%(1+$high-$low)))
+done
+echo ${random[@]}
+}
+function find_min_max() {
+for ((i = 0; i<=n; i++))
+do
+	for((j = 0; j<n-i-1; j++))
+	do
+		if [ ${random[j]} -gt ${random[$((j+1))]} ]
+		then
+			# swap
+			temp=${random[j]}
+			random[$j]=${random[$((j+1))]}
+			random[$((j+1))]=$temp
+		fi
+	done
+done
+		echo "Second smallest in the array: " ${random[0+1]}
+		echo "Second largest in the array: " ${random[n-2]}
+}
+n=10;
+arraygen
+find_min_max
